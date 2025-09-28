692d63c44d77a07f3e089d3a2fae3424f0197db4
reissada <pedro.reis_sa_da_costa@nokia.com>
2025-09-28 13:38:53 +0300

    update conf

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,65 +19,115 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local appname = vim.env.NVIM_APPNAME or "nvim"
-local profile = (appname == "nvim-devops") and "devops" or "main"
+local profile = (vim.env.NVIM_PROFILE or appname):lower()
+profile = profile:gsub("^nvim%-", "")
+if profile == "" or profile == "nvim" then
+  profile = "main"
+end
+
+local function ensure_tools(opts, tools)
+  opts.ensure_installed = opts.ensure_installed or {}
+  for _, tool in ipairs(tools) do
+    if not vim.tbl_contains(opts.ensure_installed, tool) then
+      table.insert(opts.ensure_installed, tool)
+    end
+  end
+end
+
+local function preferred_files_cmd()
+  if vim.fn.executable("fd") == 1 then
+    return {
+      "fd",
+      "--type",
+      "f",
+      "--type",
+      "l",
+      "--hidden",
+      "--follow",
+      "--color",
+      "never",
+      "--exclude",
+      ".git",
+    }
+  end
+
+  return {
+    "rg",
+    "--files",
+    "--hidden",
+    "--follow",
+    "--glob",
+    "!.git/*",
+  }
+end
+
+local mason_common_tools = {
+  "lua-language-server",
+  "stylua",
+  "bash-language-server",
+  "shfmt",
+  "shellcheck",
+  "marksman",
+}
+
+local mason_go_tools = {
+  "gopls",
+  "delve",
+  "golangci-lint",
+  "gofumpt",
+  "goimports-reviser",
+  "golines",
+  "gomodifytags",
+  "iferr",
+  "impl",
+  "gotests",
+  "yaml-language-server",
+  "yamlfmt",
+  "yamllint",
+  "helm-ls",
+  "hadolint",
+}
 
 local specs = {
-    -- LazyVim core + plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.util.project" },
+  -- LazyVim core + plugins
+  { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+  { import = "lazyvim.plugins.extras.ui.mini-animate" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.util.project" },
 
-    -- Common plugins you want everywhere (optional “plugins” dir if you use it)
-    { import = "plugins" },
-    {
-      "stevearc/oil.nvim",
-      opts = { default_file_explorer = true, view_options = { show_hidden = true } },
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      keys = {
-        { "-", "<CMD>Oil<CR>", desc = "Open parent directory (oil)" },
-      },
+  -- Common plugins you want everywhere (optional "plugins" dir if you use it)
+  { import = "plugins" },
+  {
+    "stevearc/oil.nvim",
+    opts = { default_file_explorer = true, view_options = { show_hidden = true } },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "-", "<CMD>Oil<CR>", desc = "Open parent directory (oil)" },
     },
-    {
-      "nvim-telescope/telescope.nvim",
-      opts = {
-        defaults = {
-          find_command = {
-            "fd",
-            "--type",
-            "f",
-            "--type",
-            "l",
-            "--color",
-            "never",
-            "--hidden",
-            "-E",
-            ".git",
-          },
-        },
-      },
-    },
-  }
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.defaults = opts.defaults or {}
+      opts.defaults.find_command = preferred_files_cmd()
+      return opts
+    end,
+  },
+}
 
-if profile == "main" then
-  vim.list_extend(specs, {
+local profile_specs = {
+  main = {
+    -- Go IDE
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.test.core" },
     {
       "mason-org/mason.nvim",
       opts = function(_, opts)
         opts = opts or {}
-        opts.ensure_installed = opts.ensure_installed or {}
-        for _, t in ipairs({
-          "lua-language-server",
-          "stylua",
-          "bash-language-server",
-          "shfmt",
-          "shellcheck",
-          "marksman",
-        }) do
-          if not vim.tbl_contains(opts.ensure_installed, t) then
-            table.insert(opts.ensure_installed, t)
-          end
-        end
+        ensure_tools(opts, mason_common_tools)
+        ensure_tools(opts, mason_go_tools)
         return opts
       end,
     },
@@ -107,79 +157,168 @@ if profile == "main" then
         picker = {
           sources = {
             files = {
-              cmd = function()
-                if vim.fn.executable("fd") == 1 then
-                  return {
-                    "fd",
-                    "--type",
-                    "f",
-                    "--type",
-                    "l",
-                    "--hidden",
-                    "--follow",
-                    "--color",
-                    "never",
-                    "--exclude",
-                    ".git",
-                  }
-                end
-                return {
-                  "rg",
-                  "--files",
-                  "--hidden",
-                  "--follow",
-                  "--glob",
-                  "!.git/*",
-                }
-              end,
+              cmd = preferred_files_cmd,
             },
           },
         },
       },
     },
+
     {
-      "nvim-telescope/telescope.nvim",
-      optional = true,
+      "ray-x/go.nvim",
+      ft = { "go", "gomod", "gotmpl" },
+      dependencies = { "ray-x/guihua.lua" },
       opts = {
-        defaults = {
-          find_command = {
-            "fd",
-            "--type",
-            "f",
-            "--type",
-            "l",
-            "--hidden",
-            "--follow",
-            "--color",
-            "never",
-            "--exclude",
-            ".git",
-          },
-        },
+        gofmt = "gofumpt",
+        fillstruct = "gopls",
+        diagnostic = { virtual_text = true },
+        lsp_cfg = false, -- let LazyVim/lspconfig own LSP servers
       },
-      init = function()
-        if vim.fn.executable("fd") ~= 1 then
-          local ok, telescope = pcall(require, "telescope")
-          if ok then
-            telescope.setup({
-              defaults = {
-                find_command = {
-                  "rg",
-                  "--files",
-                  "--hidden",
-                  "--follow",
-                  "--glob",
-                  "!.git/*",
-                },
-              },
-            })
-          end
-        end
+      config = function(_, opts)
+        require("go").setup(opts)
+        vim.g.go_build_tags = ""
       end,
     },
-  })
-elseif profile == "devops" then
-  table.insert(specs, { import = "profiles.devops" })
+    {
+      "leoluz/nvim-dap-go",
+      ft = { "go" },
+      dependencies = { "mfussenegger/nvim-dap" },
+      config = function()
+        require("dap-go").setup()
+      end,
+    },
+
+    -- Helm & YAML/Kubernetes
+    { "towolf/vim-helm", ft = { "helm", "yaml", "tpl" } },
+    {
+      "someone-stole-my-name/yaml-companion.nvim",
+      ft = { "yaml", "yml" },
+      dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+      config = function()
+        local yc = require("yaml-companion")
+        local cfg = yc.setup({
+          lspconfig = {
+            settings = {
+              yaml = {
+                format = { enable = true },
+                validate = true,
+                hover = true,
+                completion = true,
+                schemas = yc.get_buf_schema(0),
+                schemaStore = { enable = true },
+                schemaDownload = { enable = true },
+                kubernetes = { enabled = true },
+              },
+            },
+          },
+        })
+        require("lspconfig").yamlls.setup(cfg)
+      end,
+    },
+
+    -- Overseer tasks
+    {
+      "stevearc/overseer.nvim",
+      cmd = { "OverseerRun", "OverseerToggle", "OverseerQuickAction" },
+      opts = { templates = {} },
+      config = function(_, opts)
+        require("overseer").setup(opts)
+        local o = require("overseer")
+        o.register_template({
+          name = "Go: build current module",
+          builder = function()
+            return { cmd = { "go" }, args = { "build", "./..." }, components = { "default" } }
+          end,
+        })
+        o.register_template({
+          name = "Go: test with race",
+          builder = function()
+            return { cmd = { "go" }, args = { "test", "./...", "-race", "-v" }, components = { "default" } }
+          end,
+        })
+        o.register_template({
+          name = "kubectl: apply current file",
+          builder = function()
+            return { cmd = { "kubectl" }, args = { "apply", "-f", vim.fn.expand("%:p") }, components = { "default" } }
+          end,
+        })
+        o.register_template({
+          name = "helm: template ./chart",
+          builder = function()
+            return { cmd = { "helm" }, args = { "template", "./chart" }, components = { "default" } }
+          end,
+        })
+      end,
+    },
+
+    -- LSP servers
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          gopls = {
+            settings = {
+              gopls = {
+                usePlaceholders = true,
+                analyses = { unusedparams = true, nilness = true, unusedwrite = true, fieldalignment = false },
+                hints = { rangeVariableTypes = true, parameterNames = true, constantValues = true },
+              },
+            },
+          },
+          yamlls = {},
+          helm_ls = {},
+        },
+      },
+    },
+
+    -- Formatters
+    {
+      "stevearc/conform.nvim",
+      optional = true,
+      opts = function(_, opts)
+        opts = opts or {}
+        opts.formatters_by_ft = opts.formatters_by_ft or {}
+        opts.formatters_by_ft.go = { "gofumpt", "goimports_reviser", "golines" }
+        opts.formatters_by_ft.yaml = { "yamlfmt" }
+        opts.formatters_by_ft.helm = { "yamlfmt" }
+        return opts
+      end,
+    },
+
+    -- Linters
+    {
+      "mfussenegger/nvim-lint",
+      optional = true,
+      opts = function(_, opts)
+        opts = opts or {}
+        opts.linters_by_ft = opts.linters_by_ft or {}
+        opts.linters_by_ft.go = { "golangci_lint" }
+        opts.linters_by_ft.yaml = { "yamllint" }
+        opts.linters_by_ft.dockerfile = { "hadolint" }
+        return opts
+      end,
+    },
+
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function()
+        local ok, configs = pcall(require, "nvim-treesitter.configs")
+        if not ok then
+          vim.notify("nvim-treesitter is not installed", vim.log.levels.WARN)
+          return
+        end
+        configs.setup({
+          ensure_installed = { "go", "gomod", "gosum", "gowork", "lua", "vim", "query" },
+          highlight = { enable = true },
+        })
+      end,
+    },
+  },
+}
+
+for _, plugin in ipairs(profile_specs[profile] or {}) do
+  table.insert(specs, plugin)
 end
 
 require("lazy").setup({
@@ -213,7 +352,7 @@ require("lazy").setup({
         "tohtml",
         "tutor",
         "zipPlugin",
-        -- enable “matchit/matchparen/netrw” only if you need them
+        -- enable "matchit/matchparen/netrw" only if you need them
       },
     },
   },
