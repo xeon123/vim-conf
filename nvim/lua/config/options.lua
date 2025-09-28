80f8c30a991bf9a2ac0f9017d2170312c78e5edc
reissada <pedro.reis_sa_da_costa@nokia.com>
2025-09-28 10:27:53 +0300

    updated conf

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.clipboard = "unnamedplus"
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 400
+vim.opt.ttimeoutlen = 10
+vim.opt.updatetime = 200
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 4
+vim.opt.mouse = "a"
